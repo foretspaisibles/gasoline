57ad0682f8fbc373093a26fb1b5dc524c6c365c5
Michael Grünewald <michipili@gmail.com>
2014-06-01 11:17:09 +0200

    Create unit tests for the unicode framework

---
+++ testsuite/unit-testing/testUnicode.ml
@@ -0,0 +1,22 @@
+(* TestUnicode -- Testing our unicode framework
+
+Author: Michael Grünewald
+Date: Sun Jun  1 11:15:28 CEST 2014
+
+Gasoline (https://github.com/michipili/gasoline)
+This file is part of Gasoline
+
+Copyright © 2014 Michael Grünewald
+
+This file must be used under the terms of the CeCILL-B.
+This source file is licensed as described in the file COPYING, which
+you should have received as part of this distribution. The terms
+are also available at
+http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
+
+open UnitTest
+
+let init suite =
+  ignore suite
+
+let () = with_registered_suite "Unicode" init
