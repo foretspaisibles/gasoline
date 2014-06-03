ecc12d2e5934610546f1e60860efca113df498ec
Michael Grünewald <michipili@gmail.com>
2014-06-03 12:33:31 +0200

    Unit test for the author kit

---
+++ testsuite/trip-trap/authorkit/main.ml
@@ -0,0 +1,30 @@
+(* Main -- Author kit
+
+Author: Michael Grünewald
+Date: Tue Jun  3 11:45:45 CEST 2014
+
+Gasoline (https://github.com/michipili/gasoline)
+This file is part of Gasoline
+
+Copyright © 2014 Michael Grünewald
+
+This file must be used under the terms of the CeCILL-B.
+This source file is licensed as described in the file COPYING, which
+you should have received as part of this distribution. The terms
+are also available at
+http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
+open Unicode
+open WHTML
+
+let document () =
+  html [
+    body [
+      h1 (u"My first heading");
+      p [c"My first paragraph."];
+    ]
+  ]
+
+let main () =
+  WSGML.print (document())
+
+let () = main ()
