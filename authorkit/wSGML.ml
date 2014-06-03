9cdb30787919aa5ff1103e8eecf02761b7498afa
Michael Grünewald <michipili@gmail.com>
2014-06-03 11:41:51 +0200

    Import the author toolkit

---
+++ authorkit/wSGML.ml
@@ -0,0 +1,285 @@
+(* WSGML -- Writer for SGML files
+
+Author: Michael Grünewald
+Date: Sat 21 Feb 2009 09:21:26 CET
+
+Gasoline (https://github.com/michipili/gasoline)
+This file is part of Gasoline
+
+Copyright © 2009–2014 Michael Grünewald
+
+This file must be used under the terms of the CeCILL-B.
+This source file is licensed as described in the file COPYING, which
+you should have received as part of this distribution. The terms
+are also available at
+http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
+open Unicode
+open UFormat
+
+let sgml_indent = 2
+
+(* Some constants. *)
+
+let buffer_sz = 4096	(* Random buffer size *)
+let margin_sz = 2 	(* Margins in the pretty printer *)
+
+let amp = UChar.of_char '&'
+let lt = UChar.of_char '<'
+let gt = UChar.of_char '>'
+let quot = UChar.of_char '"'
+
+let esc_amp = u"&amp;"
+let esc_lt = u"&lt;"
+let esc_gt = u"&gt;"
+let esc_quot = u"&quot;"
+let uempty = u""
+
+type cdata = ustring
+type pcdata = ustring
+type nmtoken = string
+
+let escape_loop output_ustring output_uchar s =
+  let escape_char c =
+    if c = amp then
+      output_ustring esc_amp
+    else if c = lt then
+      output_ustring esc_lt
+    else if c = gt then
+      output_ustring esc_gt
+    else if c = quot then
+      output_ustring esc_quot
+    else
+      output_uchar c
+  in
+  UString.iter escape_char s
+
+let escape s =
+  let l = UString.length s in
+  let b = UBuffer.create (3*l) in
+    escape_loop (UBuffer.add_ustring b) (UBuffer.add_uchar b) s;
+    UBuffer.contents b
+
+
+type snippet =
+  | CDATA of cdata
+  | PCDATA of pcdata
+  | ELEMENT of element
+
+and element  = {
+  name : nmtoken;
+  attribute : (nmtoken * cdata) Queue.t;
+  empty : bool;
+  block : bool;
+  content : snippet Queue.t;
+}
+
+let pcdata s =
+  PCDATA s
+
+let cdata s =
+  CDATA s
+
+let enqueue q l =
+  List.iter (fun x -> Queue.add x q) l
+
+let make_element n ?(empty = false) ?(block = true) ?(attr = []) c =
+  if n = "" then invalid_arg "WSGML.element";
+  let e = {
+    name = n;
+    attribute = Queue.create();
+    empty = empty;
+    block = block;
+    content = Queue.create();
+  } in
+    enqueue e.attribute attr;
+    enqueue e.content c;
+    e
+
+let element n ?empty ?block ?attr c =
+  ELEMENT(make_element n ?empty ?block ?attr c)
+
+type t = {
+  declaration: cdata;
+  dtd: cdata;
+  snips: snippet list;
+}
+
+let make ?(declaration = "") ?(dtd = "") content = {
+  declaration = u declaration;
+  dtd = u dtd;
+  snips = content;
+}
+
+let rec pp_print_snippet_loop f h d =
+  let put_uchar x =
+    if UInformation.is_space x then
+      pp_print_space f ()
+    else
+      pp_print_uchar f x
+  in
+  let put_ustring x =
+    UString.iter put_uchar x in
+  let pp_print_pcdata _ x = escape_loop put_ustring put_uchar x in
+  let pp_print_attribute f (k,v) =
+    begin
+      pp_print_space f ();
+      pp_open_hbox f ();
+      pp_print_string f k;
+      pp_print_char f '=';
+      pp_print_char f '"';
+      pp_print_ustring f v;
+      pp_print_char f '"';
+      pp_close_box f ();
+    end
+  in
+
+  let pp_print_element_open_tag f h e =
+    begin
+      pp_open_hvbox f 0;
+      pp_open_hvbox f sgml_indent;
+      pp_print_char f '<';
+      pp_print_string f e.name;
+      Queue.iter (pp_print_attribute f) e.attribute;
+      pp_close_box f ();
+      pp_print_cut f ();
+      pp_print_char f '>';
+      pp_close_box f ();
+    end
+  in
+  let pp_print_element_close_tag f h e =
+    begin
+      pp_open_hbox f ();
+      pp_print_char f '<';
+      pp_print_char f '/';
+      pp_print_string f e.name;
+      pp_print_char f '>';
+      pp_close_box f ();
+    end
+  in
+
+  let pp_print_element_content f h e =
+    begin
+      Queue.iter (pp_print_snippet_loop f e.block) e.content;
+    end
+  in
+  let pp_print_element f h e =
+    begin
+      if h then pp_print_cut f ();
+      if e.block or h then (
+	pp_open_hbox f ();
+	pp_open_vbox f 0;
+      );
+
+      (
+	if e.block then
+	  pp_open_vbox f sgml_indent
+	else if h then
+	  pp_open_hovbox f sgml_indent
+      );
+      pp_print_element_open_tag f h e;
+      if not e.empty then (
+	if e.block then pp_print_cut f ();
+	pp_print_element_content f h e;
+	if e.block then pp_print_cut f ();
+      );
+      if e.block or h then pp_close_box f ();
+      if e.block then pp_print_cut f ();
+      if e.block or h then pp_close_box f ();
+      if not e.empty then (
+	pp_print_element_close_tag f h e;
+      );
+      if h then pp_print_cut f ();
+      if e.block or h then pp_close_box f ();
+      if h then pp_print_cut f ();
+    end
+  in
+  match d with
+    | PCDATA c -> pp_print_pcdata f c
+    | CDATA c -> pp_print_ustring f c
+    | ELEMENT e -> pp_print_element f h e
+
+
+
+let uformat f d =
+  let pp_maybe_print_uline f l =
+    if not (l = uempty) then
+      begin
+	pp_open_hbox f ();
+	pp_print_ustring f l;
+	pp_close_box f ();
+	pp_print_cut f ();
+      end
+  in
+  let pp_print_snippet f s =
+    begin
+      pp_open_hbox f ();
+      pp_print_snippet_loop f false s;
+      pp_close_box f ();
+      pp_print_cut f ();
+    end
+  in
+  begin
+    pp_open_vbox f 0;
+    pp_maybe_print_uline f d.declaration;
+    pp_maybe_print_uline f d.dtd;
+    List.iter (pp_print_snippet f) d.snips;
+    pp_close_box f ();
+  end
+
+let format ?enc f d =
+  uformat (formatter_of_formatter ?enc f) d
+
+let add_to_buffer ?enc b d =
+  uformat (formatter_of_buffer ?enc b) d
+
+let to_buffer ?enc d =
+  let b = Buffer.create 4096 in
+  begin
+    add_to_buffer ?enc b d;
+    b;
+  end
+
+let to_string ?enc d =
+  Buffer.contents (to_buffer ?enc d)
+
+let output ?enc c d =
+  uformat (formatter_of_out_channel ?enc c) d
+
+let print ?enc d =
+  output ?enc stdout d
+
+let write ?enc f d =
+  let c = open_out f in
+  begin
+    output ?enc c d;
+    close_out c;
+  end
+
+let concat = List.concat
+
+
+let rec outcome_loop x l =
+  match l with
+    | (_, None)::t -> outcome_loop x t
+    | (f, Some v)::t -> outcome_loop ((f v)::x) t
+    | [] -> List.rev x
+
+let outcome l =
+  outcome_loop [] l
+
+
+type factory = element
+
+let prepare x =
+  match x with
+    | ELEMENT e -> e
+    | _ -> invalid_arg "prepare"
+
+let add_attribute f a =
+  Queue.add a f.attribute
+
+let add_content f c =
+  Queue.add c f.content
+
+let finalize f =
+  ELEMENT f
---
+++ authorkit/wSGML.mli
@@ -0,0 +1,148 @@
+(* WSGML -- Writer for SGML files
+
+Author: Michael Grünewald
+Date: Sat 21 Feb 2009 09:21:26 CET
+
+Gasoline (https://github.com/michipili/gasoline)
+This file is part of Gasoline
+
+Copyright © 2009–2014 Michael Grünewald
+
+This file must be used under the terms of the CeCILL-B.
+This source file is licensed as described in the file COPYING, which
+you should have received as part of this distribution. The terms
+are also available at
+http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
+(** Simple writer for SGML files
+
+We define rudimentary facilities to assemble SGML files. These facilities are
+organised under the notion of {i snippet}. A snippet represents a
+well-balanced fragment of a hypothetical SGML document (the document being
+assembled). Once they are created, snippets can be pasted together to build
+larger snippets. Ultimately, the final snippet is rendered in a buffer,
+a string, or output on a channel stream. *)
+
+
+(** {2 Text occuring in snippets}
+
+The text occuring in document snippets comes in several flavours. All of these
+are unicode strings, but they differ in the way they are used. *)
+
+type cdata = Unicode.ustring
+(** The type of {i character data}. This kind of data is pasted into the
+output without further processing. This type is typically used for giving
+titles, identifiers, etc. to elements. *)
+
+type pcdata = Unicode.ustring
+(** The type of {i parsed character data}. This kind of data is further
+processed before pasting, in particulat SGML reserved characters are
+escaped. *)
+
+type nmtoken = string
+(** The type of {i name tokens}. This kind of data is used to labele elements
+and keys of their attributes. *)
+
+val escape: pcdata -> cdata
+(** [escape s] returns an escaped form for [s], ready to be pasted in the
+final document .*)
+
+type snippet
+(** The type of {i snippets}. A snippet can be a piece of text or an element,
+as described below. *)
+
+val cdata : cdata -> snippet
+(** Create a simple snippet containing free character data. *)
+
+val pcdata: pcdata -> snippet
+(** Create a simple snippet containing parsed character data. *)
+
+type element
+(** The type of {i elements}. Elements have a name, a list of attributes and a
+content. The empty field tells the renderer wether the closing tag may be
+omitted and the block field is a rendering hint distinguishing block level
+elements of text levels. Note that this vision of SGML elements is rather
+approximative!  *)
+
+val element : nmtoken -> ?empty:bool -> ?block:bool ->
+  ?attr:(nmtoken * cdata) list -> snippet list -> snippet
+(** [element name content] create a complex snippet, consisting of an element
+named [name] containing the snippets specified in [content].
+
+@raise Invalid_arg if [name] is the empty string. *)
+
+(** {2 Pasting snippets} *)
+
+(** The formatting functions below that require an encoding will fail
+if the given encoding is not able to represent all the characters
+found in the document being formatted. *)
+
+type t
+(** The type of {i final documents}. *)
+
+val make : ?declaration:nmtoken -> ?dtd:nmtoken -> snippet list -> t
+(** [make content] create a document snippet with the specified
+content. It is possible to preprend a declaration and a
+DTD identifier to the resulting document. *)
+
+val uformat : Unicode.UFormat.formatter -> t -> unit
+(** [format f d] format document [d] to unicode formatter [f]. *)
+
+val format : ?enc:Unicode.Encoding.t -> Format.formatter -> t -> unit
+(** [format f d] format document [d] to formatter [f]. *)
+
+val add_to_buffer : ?enc:Unicode.Encoding.t -> Buffer.t -> t -> unit
+(** Format a document to an existing unicode buffer. *)
+
+val to_buffer : ?enc:Unicode.Encoding.t -> t -> Buffer.t
+(** Format a document to a fresh new buffer. *)
+
+val to_string : ?enc:Unicode.Encoding.t -> t -> string
+(** Format a document to a fresh new string. *)
+
+val output : ?enc:Unicode.Encoding.t -> out_channel -> t -> unit
+(** Write the document to the output channel. *)
+
+val print : ?enc:Unicode.Encoding.t -> t -> unit
+(** Write the document to the standard output. This uses the Unicode's module
+presentation of standard output, so encoding can be set using
+[Unicode.Uchannel] module. *)
+
+val write : ?enc:Unicode.Encoding.t -> string -> t -> unit
+(** Turn the document into an encoded file. This may fail if the required
+encoding does not support all of the characters occuring in the document. *)
+
+(** {2 Factory}
+
+We provide a few services in order to simplify the elaboration of complex
+documents. *)
+
+val concat: snippet list list -> snippet list
+(** This is an avatar of [List.concat]. It is useful when preparing the
+content of an element. *)
+
+val outcome: (('a -> 'b) * ('a option)) list -> 'b list
+(** [outcome [(f1, opt1); ...]] return the list deduced from its argument by:
+filtering out pairs having [None] as second member, and replacing [(f,Some v)]
+by [f v].
+
+This procedure may be useful when writing a function with a big number of
+optional arguments, describing optional parts of a document. *)
+
+type factory
+(** The type of {element factories}. An element factory allows the foregery of
+complex elements. *)
+
+val prepare : snippet -> factory
+(** [prepare s] create a complex snippet factory, base of the
+snippet [s].
+
+@raise Invalid_argument "prepare" if [s] is not an element. *)
+
+val add_content: factory -> snippet -> unit
+(** Add a snippet to the element under prepareation. *)
+
+val add_attribute: factory -> (nmtoken * cdata) -> unit
+(** Add an atribute to the element under preparation. *)
+
+val finalize : factory -> snippet
+(** Finish the preparation of the snippet. *)
