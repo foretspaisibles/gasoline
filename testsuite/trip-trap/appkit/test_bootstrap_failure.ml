43879ed4b69e625630cbc03ba29f418e67fd365e
Michael Grünewald <michipili@gmail.com>
2014-09-21 00:56:49 +0200

    Prepare tests for the application kit

---
+++ testsuite/trip-trap/appkit/test_bootstrap_failure.ml
@@ -0,0 +1,41 @@
+(* Test_bootstrap_failure -- Test bootstrap failure
+
+Author: Michael Grünewald
+Date: Sun Sep 21 00:26:23 CEST 2014
+
+Gasoline (https://github.com/michipili/gasoline)
+This file is part of Gasoline
+
+Copyright © 2014 Michael Grünewald
+
+This file must be used under the terms of the CeCILL-B.
+This source file is licensed as described in the file COPYING, which
+you should have received as part of this distribution. The terms
+are also available at
+http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
+module Application =
+  CApplication
+
+module Monolith =
+struct
+  let comp = {
+    Application.Component.
+    name = "monolith";
+    version = "1.0";
+    require = [];
+    provide = [];
+    description = "The monolithic component of our application";
+    config_prefix = [];
+    getopt_prefix = None;
+  }
+
+  let bootstrap _ =
+    failwith "This error was triggered on purpose."
+
+  let () = Application.Component.register ~bootstrap comp
+end
+
+let () =
+  Application.run
+    "test_boostrap_failure" "" ""
+    ignore
---
+++ testsuite/trip-trap/appkit/test_execution_failure.ml
@@ -0,0 +1,22 @@
+(* Test_execution_failure -- Test execution failure
+
+Author: Michael Grünewald
+Date: Sun Sep 21 00:26:39 CEST 2014
+
+Gasoline (https://github.com/michipili/gasoline)
+This file is part of Gasoline
+
+Copyright © 2014 Michael Grünewald
+
+This file must be used under the terms of the CeCILL-B.
+This source file is licensed as described in the file COPYING, which
+you should have received as part of this distribution. The terms
+are also available at
+http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt *)
+module Application =
+  CApplication
+
+let () =
+  Application.run
+    "test_execution_failure" "" ""
+    (fun _ -> failwith "This error was triggered on purpose.")
